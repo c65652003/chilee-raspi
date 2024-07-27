162ced93db97a45a4f04f06dab12db99adfabd64
5a0585 <c65652003@gmail.com>
2024-07-27 09:20:12 +0100

    1620

--- Lesson0727.py
+++ Lesson0727.py
@@ -1,15 +1,37 @@
-#dictionary
-#dict
+import os.path
+import os
+from datetime import datetime
+import random
 
-student_dict = {"name":"robert",
-                "chinese":87,
-                "english":97,
-                "math":86}
 
-##print(type(student_dict))
+current_path = os.path.abspath(__name__) #取得目前檔案路徑
+directory_name = os.path.dirname(current_path) #取得目前資料夾路行
+data_path = os.path.join(directory_name,'data') #目前資料夾路徑加上data目錄
+print(data_path)
+if not os.path.isdir(data_path):
+    print("沒有data的目錄,手動建立目錄")
+    os.mkdir(data_path)
+else:
+    print("目錄已經建立")
 
-#subscript
-##student_dict["name"]
-##print(student_dict)
+log_path = os.path.join(data_path,'iot.log')
+if not os.path.isfile(log_path):
+    print("沒有iot.log檔,建立新檔")
+    with open(log_path,mode='w',encoding='utf-8',newline='') as file:
+        file.write('時間,濕度,溫度\n')
+    
+else:
+    print("已經有log檔")
 
-student_dict.values()
+
+
+now = datetime.now()
+now_str = now.strftime("%Y-%m-%d %H:%M:%S")
+humidity = str(random.randint(330,820) / 10)
+celsius = str(random.randint(50,400) / 10)
+with open(log_path,mode='a',encoding='utf-8',newline='') as file:
+    file.write(now_str + ',' + humidity + ',' + celsius + "\n")
+
+##import random
+humidity = str(random.randint(330,820) / 10)
+celsius = str(random.randint(50,400) / 10)
